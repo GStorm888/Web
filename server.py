ecb9ada930e9da793dfc13211eed6c7c6d2b4393
GStorm888 <denchikevstr@icloud.com>
2024-12-29 13:16:06 +0300

    UpdateB

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def ruwiki():
+    return render_template('ruwiki.html')
+
+@app.route('/hello')
+def hello():
+    return "Hello world!"
+
+@app.route("/max")
+def find_max():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    if a > b:
+        return f"<h1> максимум это число а: {a}</h1>"
+    else:
+        return f"<h1> максимум это число b: {b}</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
